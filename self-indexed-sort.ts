2319a77f392a801758fa1c2392ebdbc68b27c575
Can Umay <30483006+canumay@users.noreply.github.com>
2021-01-10 17:43:02 +0300

    algorithm updates

--- self-indexed-sort.ts
+++ self-indexed-sort.ts
@@ -6,33 +6,23 @@ const selfIndexSort = (arr: Array<number>): Array<number> => {
     if (el > maxNum) maxNum = el;
     if (el < minNum) minNum = el;
   });
-  console.log("min element of array:", minNum);
-  console.log("max element of array:", maxNum);
 
   const SSCOUNT: number = maxNum - minNum + 1;
 
-  console.log("Space count:", SSCOUNT);
-
   let sortSpace: Array<number> = new Array(SSCOUNT).fill(0);
 
-  console.log("Sort space before indexing:", sortSpace);
-
-  arr.forEach((el) => { 
-    sortSpace[el - minNum]++; 
+  arr.forEach((el) => {
+    sortSpace[el - minNum]++;
   });
-
-  console.log("Sort space after indexing:", sortSpace);
 
   let i: number = 0;
   for (let index = 0; index < SSCOUNT; index++) {
     while (sortSpace[index] > 0) {
       arr[i++] = index + minNum;
-      sortSpace[index]--; 
+      sortSpace[index]--;
     }
   }
   return arr;
 };
 
-const unSortedArr: Array<number> = [3, 4, 1000, 7, 4];
-console.log("Array before self indexed sort", unSortedArr);
-console.log("Array after self indexed sort", selfIndexSort(unSortedArr));
+export default selfIndexSort;
